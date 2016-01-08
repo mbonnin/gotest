ffba3d6731167808305045a26732ecb33fe72f0e
Mark Bonnin <mbonnin@gmail.com>
2016-01-07 19:03:04 -0500

    Added conversion test

--- hello.go
+++ hello.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // This is a comment
 func main() {
-    fmt.Printf("hello, world\n")
+	fmt.Printf("hello, world\n")
+
+	conversion()
+}
+
+func conversion() {
+	i := 42
+	f := float64(42)
+	u := uint(f)
+
+	fmt.Println(i, f, u)
+
 }
